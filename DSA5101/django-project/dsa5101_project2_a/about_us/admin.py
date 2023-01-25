a72662ad8cde4e13491530e747dbb9f5978d306e
YOUYANG ZHANG <youyangzhang@zyy-mbp-14.local>
2023-01-25 18:41:45 +0800

    delete current folders in repo

--- DSA5101/django-project/dsa5101_project2_a/about_us/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Movie
-
-
-class MovieAdmin(admin.ModelAdmin):
-    list_display = ('name', 'duration', 'grade')
-
-
-admin.site.register(Movie, MovieAdmin)
